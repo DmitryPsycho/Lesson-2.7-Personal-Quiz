c44149b76b974bf8de14a0d046937ef29b4483b4
DmitryPsycho <kovalenko2511@bk.ru>
2022-07-04 20:47:13 +0300

    Initial Commit

---
+++ Lesson 2.7 Personal Quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson 2.7 Personal Quiz
+//
+//  Created by Psycho on 04.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
